e3a269ddb11b3d9214ee91cc09562db4078f8184
arch-spatula <shkim516@naver.com>
2024-01-08 21:38:17 +0900

    초기버전 완성

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 (function () {
   if (!window) return;
-  window.location = 'https://github.com/';
+  window.location = 'https://asoftmurmur.com/';
 })();
-
-// "icons": {
-//   "48": "images/icon-48.svg",
-//   "128": "images/icon-128.svg"
-// },
